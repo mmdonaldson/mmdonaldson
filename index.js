41ce164aaa9fa0b812b845f82550769b95011e8b
Mitchell Donaldson <mitchell.donaldson@pacificmags.com.au>
2016-11-25 17:03:10 +1100

    Intial 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var ReactTooltip = require('react-tooltip');
 var Tooltip = React.createClass({
   render: function() {
     return (
-      <div style={{width: "200px", height: "100vh", margin: '0 auto'}}>
+      <div style={{width: "200px", height: "100vh", margin: '0 auto', position: 'absolute', zIndex: '200', top: '200'}}>
         <a data-tip="React-tooltip"> ◕‿‿◕ </a>
         <ReactTooltip place="top" type="dark" effect="float"/>
       </div>
